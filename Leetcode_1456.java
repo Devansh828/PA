cda3bada5c230998712866562b43ac45b432fe40
Devansh828 <goyaldevansh828@gmail.com>
2026-07-06 17:50:27 +0530

    Day1

---
+++ Leetcode_1456.java
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxVowels(string s, int k) {
+        int window=0;
+        int start=0;
+        int end=k;
+        for(int i=start;i<end;i++){
+            if( (s[i]=='a') || (s[i]=='e') ||  (s[i]=='i') || (s[i]=='o') || (s[i]=='u'))
+            window+=1;
+        }
+        int ans=window;
+        for(int i=end;i<s.size();i++){
+            if( (s[i]=='a') || (s[i]=='e') ||  (s[i]=='i') || (s[i]=='o') || (s[i]=='u'))
+            window+=1;
+            if( (s[start]=='a') || (s[start]=='e') ||  (s[start]=='i') || (s[start]=='o') || (s[start]=='u'))
+            window-=1;
+            ans=max(window,ans);
+            start++;
+        }
+        return ans;
+    }
+};
