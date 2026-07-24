9f1b3ce2f3397d81641d5cfe622ec1804065060e
Devansh828 <goyaldevansh828@gmail.com>
2026-07-24 17:33:49 +0530

    variableSlidingWindow

---
+++ Leetcode_209.java
@@ -0,0 +1,19 @@
+public class Leetcode_209 {
+    public int minSubArrayLen(int k, int[] arr) {
+        int left=0;
+        int sum=0;
+        int minLength=Integer.MAX_VALUE;
+        for(int right=0;right<arr.length;right++){
+            sum+=arr[right];
+            while(sum>=k){
+                minLength=Math.min(minLength,right-left+1);
+                sum-=arr[left];
+                left++;
+            }
+            
+        }
+        if(minLength==Integer.MAX_VALUE)
+        return 0;
+        return minLength;
+    }
+}
