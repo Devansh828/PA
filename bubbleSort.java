bddccf2a696648e3d84ba2ae313ffe27eae1bd80
Devansh828 <goyaldevansh828@gmail.com>
2026-07-27 16:52:53 +0530

    Bubble

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class bubbleSort {
-    public static void bubbleSort(int[]arr){
+    public static void bubbleSortAsc(int[]arr){
         int n=arr.length;
         for(int i=0;i<n-1;i++){
             for(int j=0;j<n-i-1;j++){
@@ -13,11 +13,27 @@ public class bubbleSort {
             }
         }
     }
+
+    public static void bubbleSortDesc(int[]arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]<arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
     
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		int[] arr={5,3,8,4,2};
-		bubbleSort(arr);
+		bubbleSortAsc(arr);
+		System.out.println(Arrays.toString(arr));
+        
+		bubbleSortDesc(arr);
 		System.out.println(Arrays.toString(arr));
 	}
 }
