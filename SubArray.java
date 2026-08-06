e8b1be65835c0228e862322092dcb826f0ec3636
Devansh828 <goyaldevansh828@gmail.com>
2026-08-06 18:38:26 +0530

    SubArray

---
+++ SubArray.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Main
+{
+    static List<List<Integer>> ans=new ArrayList<>();
+    static List<List<Integer>> ans1=new ArrayList<>();
+    
+    
+    static void solve(int[] nums,int index,List<Integer> current){
+        if(index==nums.length){
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+        // current.add(nums[index]);
+        solve(nums,index+1,current);
+        current.add(nums[index]);
+        // current.remove(current.size()-1);
+        solve(nums,index+1,current);
+        current.remove(current.size()-1);
+    }
+    
+    
+    
+    static void solve1(int[] nums,int index,List<Integer> current){
+        if(index==-1){
+            ans1.add(new ArrayList<>(current));
+            return;
+        }
+        // current.add(nums[index]);
+        solve1(nums,index-1,current);
+        current.add(nums[index]);
+        // current.remove(current.size()-1);
+        solve1(nums,index-1,current);
+        current.remove(current.size()-1);
+    }
+    
+	public static void main(String[] args) {
+		
+		int nums[]={1,2,3};
+		
+// 		solve(nums,0,new ArrayList<>());
+		solve1(nums,nums.length-1,new ArrayList<>());
+		
+		System.out.print(ans1);
+		
+		
+		
+	}
+}
